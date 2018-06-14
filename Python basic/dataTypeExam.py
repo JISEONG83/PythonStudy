818049d74fc175a4f8c856b862f48eb18a04f948
jiseong83 <farland83@gmail.com>
2018-06-14 21:41:53 +0900

    Upload DatatypeExam

---
+++ Python basic/dataTypeExam.py
@@ -0,0 +1,18 @@
+
+print(int(3.5))
+print(2e3)
+print(float("1.6"))
+print(float("inf"))
+print(bool(0))
+a = None
+print(a is None)
+
+v = 2+3j
+u = 1+4j
+sum_v = v+u
+mux_v = v*u
+print(sum_v.real)
+print(sum_v.imag)
+
+print(mux_v.real)
+print(mux_v.imag)
