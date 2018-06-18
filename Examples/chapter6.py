1ae2dba3c40ec47fed719d6f1e56cc2bacd77e22
johnchoi83 <johnchoi83@gmail.com>
2018-06-18 14:33:18 +0900

    chapter4,6,7

---
+++ Examples/chapter6.py
@@ -0,0 +1,21 @@
+import math
+
+# 1. indent
+x = 1
+
+if x > 0:
+    a = 1
+    b = 2
+    c = a + b
+
+else:
+    a = -1
+    b = -2
+    c = a - b
+
+print(c)
+
+# 2. library
+n = math.sqrt(9.0)
+
+print(n)
